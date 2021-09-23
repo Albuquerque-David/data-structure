2853f2fbcffb5c8c3f200d6ecd14dd1042cb68d1
David Albuquerque <davidra_rj@hotmail.com>
2021-09-23 12:26:54 -0300

    finished avl-tree

--- avl-tree/avl-tree.c
+++ avl-tree/avl-tree.c
@@ -9,39 +9,51 @@ typedef struct Node {
 } Node;
  
 Node *addNode(int key);
-int max(int a, int b);
 int height(Node *N);
 Node *insert(int value, Node *node);
 int getBalance(Node *node);
 Node *rightRotate(Node *n);
 Node *leftRotate(Node *n);
 void printTree(Node *node);
- 
+void freeTree(Node *node);
 
-int height(struct Node *N) {
-    if (N == NULL)
-        return 0;
-    return N->height;
-}
 
 int main() {
     int value;
     struct Node *root = NULL;
-    while(1) {
-        scanf("%d", &value);
-        if(value == -1)
-            break;
-        root = insert(value,root);
+    char str[100];
+
+    while(!feof(stdin)) {
+        scanf("%s",str);
+        if(str[0] == 'p') {
+            printTree(root);
+            printf("\n");
+        }
+
+        if(str[0] == 'i') {
+            if(scanf("%d",&value) == 1) {
+                root = insert(value,root);
+            }
+            
+        }
     }
-    
-    printTree(root);
+
+    freeTree(root);
 
     return 0;
 }
 
+void freeTree(Node *root) {
+    if(root != NULL) {
+        freeTree(root->left);
+        freeTree(root->right);
+        free(root);
+    }
+}
+
 void printTree(Node *root) {
     if(root != NULL) {
-        printf("%d ", root->value);
+        printf("%d(%d) ", root->value,root->height);
         printTree(root->left);
         printTree(root->right);
     }
@@ -53,12 +65,14 @@ Node* addNode(int value) {
     node->value = value;
     node->left = NULL;
     node->right = NULL;
-    node->height = 0;
+    node->height = 1;
     return node;
 
 }
 
 Node* insert(int value, Node *node) {
+
+    int balance;
 
     if( node == NULL ) {
         return addNode(value);
@@ -76,6 +90,67 @@ Node* insert(int value, Node *node) {
         node->right = insert(value,node->right);
     }
 
+    node->height = ((height(node->left) > height(node->right)) ? height(node->left) : height(node->right)) + 1;
+
+    balance = getBalance(node);
+
+    if(balance > 1 && value < node->left->value) {
+        return rightRotate(node);
+    }
+
+    if(balance < -1 && value > node->right->value) {
+        return leftRotate(node);
+    }
+
+    if(balance > 1 && value > node->left->value) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    if(balance < -1 && value < node->right->value) {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
     return node;
 }
 
+Node *rightRotate(Node *n) {
+    Node *x = n->left;
+    Node *T2 = x->right;
+
+    x->right = n;
+    n->left = T2;
+
+    n->height = ((height(n->left) > height(n->right)) ? height(n->left) : height(n->right)) + 1;
+    x->height = ((height(x->left) > height(x->right)) ? height(x->left) : height(x->right)) + 1;
+
+    return x;
+}
+
+Node *leftRotate(Node *n) {
+    Node *x = n->right;
+    Node *T2 = x->left;
+
+    x->left = n;
+    n->right = T2;
+
+    n->height = ((height(n->left) > height(n->right)) ? height(n->left) : height(n->right)) + 1;
+    x->height = ((height(x->left) > height(x->right)) ? height(x->left) : height(x->right)) + 1;
+
+    return x;
+}
+
+int height(Node *N)
+{
+    if (N == NULL)
+        return 0;
+    return N->height;
+}
+
+int getBalance(Node *N)
+{
+    if (N == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
+}
